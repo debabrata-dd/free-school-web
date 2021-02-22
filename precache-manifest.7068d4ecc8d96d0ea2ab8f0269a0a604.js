126071e27c4a3c14e812c9f59f4b00e5532bd46e
Debabrata Das <debabrata.dd@gmail.com>
2021-02-22 11:04:10 +0530

    Updates

---
+++ precache-manifest.7068d4ecc8d96d0ea2ab8f0269a0a604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0767b2f576f627fdde919f87f1e876",
+    "url": "/free-school-web/index.html"
+  },
+  {
+    "revision": "1bd4a117fec9d2fc647f",
+    "url": "/free-school-web/static/css/main.da928e7f.chunk.css"
+  },
+  {
+    "revision": "d4aed962c0d4b827e26c",
+    "url": "/free-school-web/static/js/2.9a782803.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/free-school-web/static/js/2.9a782803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bd4a117fec9d2fc647f",
+    "url": "/free-school-web/static/js/main.e552d175.chunk.js"
+  },
+  {
+    "revision": "185e87d5dbcaec8121c4",
+    "url": "/free-school-web/static/js/runtime-main.e0203c30.js"
+  }
+]);
